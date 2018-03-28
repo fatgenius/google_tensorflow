fd807dbb42bab6cf85e27f203bd7e408c4aa3d58
fatgenius <dbtlive@gmail.com>
2018-03-28 16:43:47 +0200

    some comments

--- panda.py
+++ panda.py
@@ -11,9 +11,11 @@ from tensorflow.python.data import Dataset
 
 tf.logging.set_verbosity(tf.logging.ERROR)
 pd.options.display.max_rows = 10
+#pandas data format
 pd.options.display.float_format = '{:.1f}'.format
-
+#read the date  from the csv file on the internet
 california_housing_dataframe = pd.read_csv("https://storage.googleapis.com/mledu-datasets/california_housing_train.csv", sep=",")
+#just make the rondom dataset
 california_housing_dataframe = california_housing_dataframe.reindex(
                                     np.random.permutation(california_housing_dataframe.index))
 california_housing_dataframe["median_house_value"] /= 1000.0
