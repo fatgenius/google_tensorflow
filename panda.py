0be8609400fbf0c294c858b180cfdba7e6c5c8cd
fatgenius <dbtlive@gmail.com>
2018-03-29 22:44:49 +0200

    add input function

--- panda.py
+++ panda.py
@@ -20,3 +20,25 @@ california_housing_dataframe = california_housing_dataframe.reindex(
                                     np.random.permutation(california_housing_dataframe.index))
 california_housing_dataframe["median_house_value"] /= 1000.0
 california_housing_dataframe.describe()
+
+#import train data
+#there are two type of training data categorical data(style) and  Numeriacla data(number like postal code)
+
+# Define the input feature: total_rooms.
+my_feature = california_housing_dataframe[["total_rooms"]]
+# Configure a numeric feature column for total_rooms.
+feature_columns = [tf.feature_column.numeric_column("total_rooms")]
+
+
+#define the target
+# Define the label.
+targets = california_housing_dataframe["median_house_value"]
+my_optimizer=tf.train.GradientDescentOptimizer(learning_rate=0.0000001)
+my_optimizer = tf.contrib.estimator.clip_gradients_by_norm(my_optimizer, 5.0)
+
+# Configure the linear regression model with our feature columns and optimizer.
+# Set a learning rate of 0.0000001 for Gradient Descent.
+linear_regressor = tf.estimator.LinearRegressor(
+    feature_columns=feature_columns,
+    optimizer=my_optimizer
+)
